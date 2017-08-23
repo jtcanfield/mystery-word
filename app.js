ed9853fdf548ed66cf06a28b5e478cf074b844bb
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-23 15:16:37 -0400

    add signup

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ var authedUser = "";
 
 app.get("/", function (req, res) {
   if (authedUser === ""){res.redirect('/login');return}
-  res.render("index");
+  res.render("index", {username : authedUser});
 });
 
 app.get("/login", function (req, res) {
@@ -73,11 +73,56 @@ app.post("/login", function (req, res) {
       }
   }});
 });
+
+
+
 app.post("/signup", function (req, res) {
-  res.redirect('/signup');
+  var validform = true;
+  if (req.body.username === undefined || req.body.password1 === undefined || req.body.password2 === undefined || req.body.email === undefined){
+    res.render('signup', {status:"One field is undefined, please try again using valid characters."});
+    return
+  }
+  if (req.body.password1.length < 4){
+    res.render('signup', {status:"Password must have at least 4 characters"});
+    return
+  }
+  if (req.body.password1 !== req.body.password2){
+    res.render('signup', {status:"Passwords do not match"});
+    return
+  }
+  if (req.body.username.length < 4){
+    res.render('signup', {status:"Username must have at least 4 characters"});
+    return
+  }
+  userDataFile.users.map((x) =>{
+    var usernamestring = x.username;
+    var emailstring = x.email;
+    if (usernamestring.toLowerCase() === req.body.username.toLowerCase()){
+      res.render('signup', {status:"Username already exists, choose another user name"});
+      validform = false;
+      return
+    }
+    if (emailstring.toLowerCase() === req.body.email.toLowerCase()){
+      res.render('signup', {status:"Email already exists. Lost your Username or Password? Email me!"});
+      validform = false;
+      return
+    }
+  });
+  if (validform === false){return};
+  if (validform === true){
+    fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
+      if (err){
+          console.log(err);
+      } else {
+        obj = JSON.parse(data);
+        obj.users.push({username: req.body.username, password: req.body.password2, email: req.body.email, currentword: ""});
+        json = JSON.stringify(obj);
+        fs.writeFile('data.json', json, 'utf8');
+    }});
+  }
+  authSession = req.body.username;
+  res.redirect('/');
 });
-
-
 app.post("/logout", function (req, res) {
   authedUser = "";
   res.redirect('/login');
