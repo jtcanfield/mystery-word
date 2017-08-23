9d26724f2462cc028119293d45f8b30d69100467
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-23 13:19:59 -0400

    clean up app.js

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const mustache = require('mustache-express');
 const bodyParser = require('body-parser');
 const app = express();
 const file = './data.json';
+const session = require('express-session');
 const fs = require('fs');
-// const main = require("./public/main.js");
-// const todosArray = ["Wash the car"];
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+app.use(session({ secret: 'this-is-a-secret-token', cookie: { maxAge: 60000, httpOnly: false}}));
+const main = require("./public/main.js");
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', './views');
@@ -17,35 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 
-//This is the initial rendering, saying to use index.mustache, and declares todosMustache
+
 app.get("/", function (req, res) {
-  fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
-      if (err){
-          console.log(err);
-      } else {
-      obj = JSON.parse(data);
-      var arrayfile = obj.todoArray;
-      var done = obj.doneArray
-      // this makes todosMustache the arrayfile that was just downloaded
-      res.render('index', { todosMustache: arrayfile,  doneMustache: done});
-  }});
-  // This will make todosMustache the todosArray
-  // res.render('index', { todosMustache: todosArray });
 });
 
-//This means that every time method="post" is called on action="/", it will add to the array and redirect the user
 app.post("/", function (req, res) {
-  var addtolist = req.body.inputtodo
-  // todosArray.push(addtolist);
-  fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
-      if (err){
-          console.log(err);
-      } else {
-      obj = JSON.parse(data); //now it an object
-      obj.todoArray.push(addtolist); //add some data
-      json = JSON.stringify(obj); //convert it back to json
-      fs.writeFile('data.json', json, 'utf8'); // write it back
-  }});
   res.redirect('/');
 });
 
@@ -53,26 +31,6 @@ app.post("/", function (req, res) {
 app.post("/:dynamic", function (req, res) {
   console.log("Well, that button worked");
   console.log(req.params.dynamic);
-  fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
-      if (err){
-          console.log(err);
-      } else {
-      obj = JSON.parse(data); //now it an object
-      // console.log(obj.todoArray);
-            // the code you're looking for
-            var needle = 'hi';
-            // iterate over each element in the array
-            for (var i = 0; i < obj.todoArray.length; i++){
-            // look for the entry with a matching `code` value
-              if (obj.todoArray[i] === req.params.dynamic){
-                // console.log(obj.todoArray[i]);
-                 // we found it
-                // obj[i].name is the matched result
-              }
-            }
-      // json = JSON.stringify(obj); //convert it back to json
-      // fs.writeFile('data.json', json, 'utf8'); // write it back
-  }});
   res.redirect('/');
 });
 
