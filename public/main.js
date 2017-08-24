208e1a27ec7952b8ae212832f2bec2e76b11b4ce
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-24 11:39:28 -0400

    add client js for missing letters

--- public/main.js
+++ public/main.js
@@ -1,2 +1,16 @@
+if (document.querySelector('h2')){
+  var word = document.querySelector('h2').textContent.split(" ");
+  var wordarray = [];
+  word.map((x) => {
+    if (x.includes("wrong") || x.includes("WRONG")){
+      wordarray.push("<span style='color:red;'>"+x[5]+"</span>");
+    } else {
+      wordarray.push(x);
+    }
+    console.log(wordarray);
+  });
+}
 var textFocus = document.querySelector('input');
-textFocus.focus();
+if (textFocus !== null){
+  textFocus.focus();
+}
