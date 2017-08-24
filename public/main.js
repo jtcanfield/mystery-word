60186768d2090518278cacb150448c346aaa0c11
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-24 11:53:54 -0400

    add endgame

--- public/main.js
+++ public/main.js
@@ -1,14 +1,17 @@
-if (document.querySelector('h2')){
-  var word = document.querySelector('h2').textContent.split(" ");
+var word = document.querySelector('h2');
+if (word){
+  var wordcontent = document.querySelector('h2').textContent.split(" ");
   var wordarray = [];
-  word.map((x) => {
+  wordcontent.map((x) => {
     if (x.includes("wrong") || x.includes("WRONG")){
-      wordarray.push("<span style='color:red;'>"+x[5]+"</span>");
+      wordarray.push("<span style='color:red;'> "+x[5]+" </span>");
     } else {
-      wordarray.push(x);
+      wordarray.push(" "+x+" ");
     }
-    console.log(wordarray);
   });
+  // console.log(wordarray);
+  // console.log(wordarray.toString().replace(',',' '));
+  word.innerHTML = wordarray.join('\u0020');
 }
 var textFocus = document.querySelector('input');
 if (textFocus !== null){
